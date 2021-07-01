6530b7bdbf3e19e5ec7c18fb46c9924ad37a388b
Marc-Olivier Titeux <marcolivier.titeux@gmail.com>
2021-07-01 22:41:02 +0200

    Update publishconf.py

--- publishconf.py
+++ publishconf.py
@@ -10,7 +10,7 @@ sys.path.append(os.curdir)
 from pelicanconf import *
 
 # If your site is available via HTTPS, make sure SITEURL begins with https://
-SITEURL = 'https://blog.machines-walking-in-the-snow.com'
+SITEURL = 'https://motiteux.github.io/machines-walking/'
 RELATIVE_URLS = True
 
 FEED_ALL_ATOM = 'feeds/all.atom.xml'
