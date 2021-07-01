8e8c65f9e45c9674062cdfcc4a97e727fbf9a2d3
Marc-Olivier Titeux <marcolivier.titeux@gmail.com>
2021-07-02 00:55:00 +0200

    Update publishconf.py

--- publishconf.py
+++ publishconf.py
@@ -1,24 +1,40 @@
 #!/usr/bin/env python
-# -*- coding: utf-8 -*- #
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
 
 # This file is only used if you use `make publish` or
 # explicitly specify it as your config file.
 
 import os
 import sys
+
 sys.path.append(os.curdir)
 from pelicanconf import *
 
-# If your site is available via HTTPS, make sure SITEURL begins with https://
-SITEURL = 'https://motiteux.github.io/machines-walking/'
-RELATIVE_URLS = True
+# Important: Changing SITEURL may break links in deploy-previews
+if os.environ.get("CONTEXT") == "production":
+    SITEURL = "https://www.machines-walking-in-the-snow.com/"
+    FEED_DOMAIN = SITEURL
+    FEED_ALL_ATOM = "feeds/all.atom.xml"
+    CATEGORY_FEED_ATOM = "feeds/{slug}.atom.xml"
+    #CLAIM_BING = os.environ.get("CLAIM_BING_PROD")
+    #STAT_COUNTER_PROJECT = os.environ.get("STAT_COUNTER_PROJECT_PROD")
+    #STAT_COUNTER_SECURITY = os.environ.get("STAT_COUNTER_SECURITY_PROD")
+    GOOGLE_ANALYTICS = os.environ.get("GOOGLE_ANALYTICS_PROD")
+    #DISQUS_SITENAME = os.environ.get("DISQUS_SITENAME")
+    #COMMENTBOX_PROJECT = os.environ.get("COMMENTBOX_PROJECT")
 
-FEED_ALL_ATOM = 'feeds/all.atom.xml'
-CATEGORY_FEED_ATOM = 'feeds/{slug}.atom.xml'
+else:
+    SITEURL = ""
+
+# MAILCHIMP_FORM_ACTION = os.environ.get("MAILCHIMP_FORM_ACTION")
+UTTERANCES_REPO = os.environ.get("UTTERANCES_REPO")
+#UTTERANCES_LABEL = os.environ.get("UTTERANCES_LABEL")
+
+RELATIVE_URLS = False
+# filetime_from_git is very slow. Use it in production only
+# to avoid slow build times during development
+PLUGINS.append("filetime_from_git")
+PLUGINS.append("sitemap")
 
 DELETE_OUTPUT_DIRECTORY = True
-
-# Following items are often useful when publishing
-
-#DISQUS_SITENAME = ""
-GOOGLE_ANALYTICS = "UA-26850780-1"
